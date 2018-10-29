2ff5f74306d3331a70ccfe2fd612533cedd55c65
Lutz Goldmann <lutz@searchink.com>
2018-10-29 15:27:22 +0100

    Fix arguments

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-in_file=$1
-out_dir=$1
+in_file=${1:-input/000000.eml}
+out_dir=${2:-output/000000}
 
-rm -r $out_dir
-mkdir $out_dir
+rm -rf $out_dir
+mkdir -p $out_dir
 
 source venv/bin/activate
 python email2pdf -i $in_file -d $out_dir -o $out_dir/email.pdf --headers 
